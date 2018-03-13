b8191bc6281486daf7ab5b7d84b7c8274f296472
stevenbrown33 <stevebrown33@me.com>
2018-03-13 11:33:31 -0600

    Setup height and width constraints, as well as anchors

--- ProgramaticConstraints/ViewController.swift
+++ ProgramaticConstraints/ViewController.swift
@@ -9,17 +9,81 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // These are replacing our "Outlets"
+    var topLeftButton = UIButton()
+    var topRightButton = UIButton()
+    var bottomLeftButton = UIButton()
+    var bottomRightButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupButtons()
+        setupButtonConstraints()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // 1- add subviews
+    func setupButtons() {
+        // set colors
+        topLeftButton.backgroundColor = .red
+        topRightButton.backgroundColor = .blue
+        bottomLeftButton.backgroundColor = .green
+        bottomRightButton.backgroundColor = .yellow
+        
+        // add targets/actions
+//        topLeftButton.addTarget(self, action: #selector(), for: .touchUpInside)
+        
+        // add the buttons as subviews of the VCs view
+        view.addSubview(topLeftButton)
+        view.addSubview(topRightButton)
+        view.addSubview(bottomLeftButton)
+        view.addSubview(bottomRightButton)
     }
-
-
+    // 2- setup each button constraint
+    func setupButtonConstraints() {
+        // DONT FORGET
+        topLeftButton.translatesAutoresizingMaskIntoConstraints = false
+        topRightButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomLeftButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomRightButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Height and Width Constraints
+        let topButtonsWidths = NSLayoutConstraint(item: topLeftButton, attribute: .width, relatedBy: .equal, toItem: topRightButton, attribute: .width, multiplier: 1, constant: 0)
+        let leftButtonsHeights = NSLayoutConstraint(item: topLeftButton, attribute: .height, relatedBy: .equal, toItem: bottomLeftButton, attribute: .height, multiplier: 1, constant: 0)
+        let bottomButtonsWidths = NSLayoutConstraint(item: bottomLeftButton, attribute: .width, relatedBy: .equal, toItem: bottomRightButton, attribute: .width, multiplier: 1, constant: 0)
+        let rightButtonsHeights = NSLayoutConstraint(item: topRightButton, attribute: .height, relatedBy: .equal, toItem: bottomRightButton, attribute: .height, multiplier: 1, constant: 0)
+        
+        view.addConstraints([topButtonsWidths, leftButtonsHeights, bottomButtonsWidths, rightButtonsHeights])
+        
+        // Top left constraints
+        let topLeftLeading = NSLayoutConstraint(item: topLeftButton, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 0)
+        let topLeftTop = NSLayoutConstraint(item: topLeftButton, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 0)
+        let topLeftTrailing = NSLayoutConstraint(item: topLeftButton, attribute: .trailing, relatedBy: .equal, toItem: topRightButton, attribute: .leading, multiplier: 1, constant: 0)
+        let topLeftBottom = NSLayoutConstraint(item: topLeftButton, attribute: .bottom, relatedBy: .equal, toItem: bottomLeftButton, attribute: .top, multiplier: 1, constant: 0)
+        
+        view.addConstraints([topLeftLeading, topLeftTop, topLeftTrailing, topLeftBottom])
+        
+        // Top right constraints
+        let topRightTrailing = NSLayoutConstraint(item: topRightButton, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: 0)
+        let topRightTop = NSLayoutConstraint(item: topRightButton, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 0)
+        let topRightBottom = NSLayoutConstraint(item: topRightButton, attribute: .bottom, relatedBy: .equal, toItem: bottomRightButton, attribute: .top, multiplier: 1, constant: 0)
+        
+        view.addConstraints([topRightTrailing, topRightTop, topRightBottom])
+        
+        // Bottom left constraints
+        let bottomLeftLeading = NSLayoutConstraint(item: bottomLeftButton, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 0)
+        let bottomLeftTrailing = NSLayoutConstraint(item: bottomLeftButton, attribute: .trailing, relatedBy: .equal, toItem: bottomRightButton, attribute: .leading, multiplier: 1, constant: 0)
+        let bottomLeftBottom = NSLayoutConstraint(item: bottomLeftButton, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
+        
+        view.addConstraints([bottomLeftLeading, bottomLeftTrailing, bottomLeftBottom])
+        
+        // Bottom right constraints
+        let bottomRightTrailing = NSLayoutConstraint(item: bottomRightButton, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: 0)
+        let bottomRightBottom = NSLayoutConstraint(item: bottomRightButton, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
+        
+        view.addConstraints([bottomRightTrailing, bottomRightBottom])
+    }
+    // 3- add targets/actions for buttons
+    // 4- add animations
 }
 
